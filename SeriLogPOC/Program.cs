9a2bde5375a17e75a80d07a3ab619da7d07b5a23
Denis Missias <denis@socialminer.com>
2019-02-21 08:41:56 -0300

    Add project files.

---
+++ SeriLogPOC/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SeriLogPOC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
